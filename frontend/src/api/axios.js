b66b16d26a3770f3c1f4b0ec922fdde40029c799
ManishaPabolta <manishapabolta24@navgurukul.org>
2026-05-25 11:13:03 +0530

    added proudction api key

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
- baseURL: "http://localhost:5000/api",
+ baseURL: "https://online-voting-system-6i81.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
