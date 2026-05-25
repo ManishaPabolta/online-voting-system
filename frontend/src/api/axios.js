becb664054c096dc26ac6f9e2f0c71a6deeca4fc
ManishaPabolta <manishapabolta24@navgurukul.org>
2026-05-25 13:38:41 +0530

    fix cors

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,30 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
- baseURL: "https://online-voting-system-6i81.onrender.com/api",
-  headers: {
-    "Content-Type": "application/json",
-  },
-   withCredentials: true,
+  baseURL:
+    "https://online-voting-system-6i81.onrender.com/api",
+
+  withCredentials: true,
 });
-
- 
-
-
-API.interceptors.request.use(
-  (config) => {
-    const token =
-      localStorage.getItem(
-        "token"
-      );
-
-    if (token) {
-      config.headers.Authorization =
-        `Bearer ${token}`;
-    }
-
-    return config;
-  }
-);
 
 export default API;
