de984da8bac5c277ed640fad6eac03f7eb47adba
ManishaPabolta <manishapabolta24@navgurukul.org>
2026-05-25 16:08:40 +0530

    Fix auth token handling and API requests

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -3,8 +3,25 @@ import axios from "axios";
 const API = axios.create({
   baseURL:
     "https://online-voting-system-6i81.onrender.com/api",
-
   withCredentials: true,
 });
+
+// 👇 ADD THIS
+API.interceptors.request.use(
+  (config) => {
+    const token =
+      localStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization =
+        `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default API;
